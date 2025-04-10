cced38ad90c2f8c6e2c046c7dcd192f277774be4
HanKyungPark <156498970+HanKyungPark@users.noreply.github.com>
2025-04-10 15:09:41 +0900

    first commit

---
+++ src/test/java/com/BackOffice/BackOffice/BackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackOffice.BackOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
