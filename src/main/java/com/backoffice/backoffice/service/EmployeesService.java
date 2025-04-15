40663ff8efd1e4e95c48a5f8f7c5245cddcd22fb
HanKyungPark <156498970+HanKyungPark@users.noreply.github.com>
2025-04-15 14:46:50 +0900

    employees CRUD 작성

--- src/main/java/com/backoffice/backoffice/service/EmployeesService.java
+++ src/main/java/com/backoffice/backoffice/service/EmployeesService.java
@@ -4,6 +4,9 @@ import com.backoffice.backoffice.dto.EmployeesDto;
 import com.backoffice.backoffice.mapper.EmployeesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,19 +15,52 @@ public class EmployeesService {
 
     private final EmployeesMapper employeesMapper;
 
-    public void save(EmployeesDto employeesDto) {
+    //사원 가입
+    public void employeesSave(EmployeesDto employeesDto) {
         try {
             // 이메일을 찾은 후, 반환된 값이 null이면 새로운 이메일로 저장
             String existingMail = employeesMapper.findByEmail(employeesDto.getEmail());
 
             // 이메일이 존재하지 않으면 저장
             if (existingMail == null) {
-                employeesMapper.save(employeesDto);
+                employeesMapper.employeesSave(employeesDto);
             } else {
                 // 이메일이 존재하면 이미 존재하는 메일로 처리
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    //사원 업데이트
+    @Transactional
+    public void employeesUpdate(EmployeesDto employeesDto) {
+        try {
+employeesMapper.employeesUpdate(employeesDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //사원 삭제
+    @Transactional
+    public void employeesDelete(EmployeesDto employeesDto) {
+        try {
+employeesMapper.employeesDelete(employeesDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //사원 찾기
+    @Transactional
+    public List<EmployeesDto> findEmployees(Integer id) {
+        return employeesMapper.findEmployees(id);
+    }
+
+    //사원 전체 출력
+    @Transactional
+    public List<EmployeesDto> findAllEmployees() {
+        return employeesMapper.findAllEmployees();
     }
 }
